033a440ddf91c3d82da202c47578bec5e8dcb837
ge1kin <aezzeaaa@gmail.com>
2026-01-11 13:53:31 +0200

    added class Coin

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hi")
+class Coin:
+    def __init__(self, center_x: str, center_y: str, value: str):
+        if not center_x.isdigit():
+            print("center x must be an integer number!")
+        if not center_y.isdigit():
+            print("center y must be an integer number!")
+        self.center_x = int(center_x)
+        self.center_y = int(center_y)
+        if not value.isdigit():
+            self.value: int = 10
+        else:
+            self.value: int = int(value)
